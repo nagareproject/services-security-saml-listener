980692db0010100e36351f8e894e527b00ec820d
Alain Poirier <alain.poirier@net-ng.com>
2021-07-20 18:51:32 +0200

    Initial checkin

---
+++ nagare/services/security/saml_listener.py
@@ -0,0 +1,38 @@
+# Encoding: utf-8
+
+# --
+# Copyright (c) 2008-2021 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+# --
+
+from nagare.services import plugin
+from nagare.sessions import common
+
+
+class Service(plugin.Plugin):
+    LOAD_PRIORITY = common.SessionsSelection.LOAD_PRIORITY - 1
+
+    def __init__(self, name, dist, services_service, **config):
+        services_service(super(Service, self).__init__, name, dist, **config)
+        self.saml_services = {}
+
+    def register_service(self, ident, saml_service):
+        self.saml_services[ident] = saml_service
+
+    def handle_request(self, chain, request, **params):
+        state = request.params.get('RelayState')
+
+        if state and state.startswith('#') and ('SAMLResponse' in request.params):
+            saml_service_ident = state.split('#')[1]
+            saml_service = self.saml_services.get(saml_service_ident)
+            if saml_service is not None:
+                is_valid, session_id, state_id = saml_service.is_auth_response(request)[:3]
+                if is_valid:
+                    params['session_id'] = session_id
+                    params['state_id'] = state_id
+
+        return chain.next(request=request, **params)
---
+++ setup.py
@@ -0,0 +1,40 @@
+# Encoding: utf-8
+
+# --
+# Copyright (c) 2008-2021 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+# --
+
+from os import path
+
+
+from setuptools import setup, find_packages
+
+here = path.normpath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst')) as long_description:
+    LONG_DESCRIPTION = long_description.read()
+
+setup(
+    name='nagare-services-security-saml-listener',
+    author='Net-ng',
+    author_email='alain.poirier@net-ng.com',
+    description='SAML listener',
+    long_description=LONG_DESCRIPTION,
+    license='BSD',
+    keywords='',
+    url='https://github.com/nagareproject/services-security-saml-listener',
+    packages=find_packages(),
+    zip_safe=False,
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
+    install_requires=['nagare-services', 'nagare-services-sessions'],
+    entry_points='''
+        [nagare.services]
+        saml_listener = nagare.services.security.saml_listener:Service
+    '''
+)
